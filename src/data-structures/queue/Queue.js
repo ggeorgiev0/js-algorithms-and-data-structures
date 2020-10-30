f73d27f9f16dbd1371ef68766c36bebddd61a67b
Georgi Georgiev <ggeorgiev@fmsystems.com>
2020-10-30 13:15:02 +0200

    Add queue and tests for it

---
+++ src/data-structures/queue/Queue.js
@@ -0,0 +1,52 @@
+import LinkedList from '../linked-list/LinkedList';
+
+export default class Queue {
+  constructor() {
+    /**
+     * Queues are similar to linked lists, 
+     * because they both operate on elements at the beginning and at the end.
+     */
+    this.linkedList = new LinkedList();
+  }
+
+  /**
+   * @return {boolean}
+   */
+  isEmpty() {
+    return !this.linkedList.head;
+  }
+
+  /**
+   * Reads the element that is in the front of the queue, without removing it.
+   * @return {*}
+   */
+  peek() {
+    if (!this.linkedList.head) return null;
+    return this.linkedList.head.value;
+  }
+
+  /**
+   * Add an element to the end of the queue (at the tail).
+   * @param {*} value
+   */
+  enqueue(value) {
+    this.linkedList.append(value);
+  }
+
+  /**
+   * Remove an element that is at the beginning of the queue (at the head).
+   * @return {*}
+   */
+  dequeue() {
+    const removehead = this.linkedList.deleteHead();
+    return removehead ? removehead.value : null;
+  }
+
+  /**
+   * @param {[cb]}
+   * @return {string}
+   */
+  toString(cb) {
+    return this.linkedList.toString(cb);
+  }
+}
---
+++ src/data-structures/queue/__test__/Queue.test.js
@@ -0,0 +1,52 @@
+import Queue from '../Queue';
+
+describe('Queue', () => {
+  let queue = null;
+  
+  beforeEach(() => {
+    queue = new Queue();
+  });
+
+  afterEach(() => {
+    queue = null;
+  });
+
+  it('should create an empty queue', () => {
+    expect(queue).not.toBeNull();
+    expect(queue.linkedList).not.toBeNull();
+  });
+
+  it('should enqueue data to the queue', () => {
+    queue.enqueue(1);
+    queue.enqueue(2);
+
+    expect(queue.toString()).toBe('1,2');
+  });
+
+  it('should be able to enqueue/dequeue objects', () => {
+    queue.enqueue({ value: 'test1', key: 'key1' });
+    queue.enqueue({ value: 'test2', key: 'key2' });
+
+    const stringifier = arg => `${arg.key}:${arg.value}`;
+
+    expect(queue.toString(stringifier)).toBe('key1:test1,key2:test2');
+    expect(queue.dequeue().value).toBe('test1');
+    expect(queue.dequeue().value).toBe('test2');
+  });
+
+  it('should be able to peek data from the queue', () => {
+    expect(queue.peek()).toBeNull();
+    queue.enqueue(1);
+    expect(queue.peek()).toBe(1);
+  });
+
+  it('should dequeue in FIFO order', () => {
+    queue.enqueue(1);
+    queue.enqueue(2);
+
+    expect(queue.dequeue()).toBe(1);
+    expect(queue.dequeue()).toBe(2);
+    expect(queue.dequeue()).toBeNull();
+    expect(queue.isEmpty()).toBe(true);
+  });
+});
